876ef44a4d9dceffdba4545d62f2a3a358937035
Matt Farncombe <matt@farncombe.co>
2020-05-09 13:20:47 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,8 @@
 def prime?(num)
-  count = 2
-  while count <= num
-    if num % count == 0
+  for i in 2..num-1
+    if num % i == 0
       return false
     end
-    count += 1
   end
   return true
 end
