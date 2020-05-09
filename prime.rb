cc99e1cb7e2e92012d913d6e8542f7bfe08a0610
Matt Farncombe <matt@farncombe.co>
2020-05-09 13:26:54 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,11 @@
 def prime?(num)
-  for i in 2..num-1
-    if num % i == 0 || num < 0
-      return false
+  if num < num
+    return false
+  else
+    for i in 2..num-1
+      if num % i == 0 || num < 0
+        return false
+      end
     end
   end
   return true
