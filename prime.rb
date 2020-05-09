5b84e7325eb84970f42d7575794874260956fc19
Matt Farncombe <matt@farncombe.co>
2020-05-09 13:31:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,9 @@ def prime?(num)
   if num <= 0
     puts "num is #{num}"
     return false
-  else
-    for i in 2..num-1
-      if num % i == 0 || num < 0
+  end
+  for i in 2..num-1
+    if num % i == 0 || num < 0
         return false
       end
     end
